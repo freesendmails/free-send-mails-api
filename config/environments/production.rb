6743da48d4382baf80738f277555f92ce1d03813
GrassiRicardo <grassiricardorg@gmail.com>
2017-07-28 15:23:52 -0300

    add config mail production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,14 +76,20 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'free-send-mail.herokuapp.com' }  
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "https://free-send-mail.herokuapp.com/"}
+  ActionMailer::Base.default :from => 'ricardograssi91@gmail.com'
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'ricardograssi91@gmail.com',
-    password:             'r.m,p.a,1',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+    :authentication => :plain,
+    :address =>  'smtp.gmail.com',
+    :port => 587,
+    :domain => 'gmail.com',,
+    :user_name => 'ricardograssi91@gmail.com',
+    :password => 'r.m,p.a,1'
+  }
+  # ActionMailer Config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
 end
