242c5ab5537853b80d220b6be074faa7471a3a55
grassiricardo <grassiricardorg@gmail.com>
2017-08-03 00:34:52 -0300

    validations parameters

--- app/controllers/v1/mails_controller.rb
+++ app/controllers/v1/mails_controller.rb
@@ -3,26 +3,30 @@ class V1::MailsController < ApplicationController
   def create_mail
     begin
 
-      #to mail
-      @to_email = params[:to]
-      #name
-      @name = params[:_name]
-      #email
-      @email = params[:_email]
-      #message email
-      @message = params[:_message]
-      #subject
-      @subject = params[:_subject] ? params[:_subject] : 'Email send by - Free Send Mails'
-      #url sucess
-      @url_success = params[:_url_success]
-
-      #send mail
-      SendMailMailer.send_mail(@to_email, @subject, @email, @message).deliver_later
-
-      if @url_success != nil
-        redirect_to @url_success
+      if params[:to] == nil || params[:_name] == nil || params[:_email] == nil || params[:_message] == nil
+        redirect_to 'http://www.freesendmails.com/test-mail-error'
       else
-        redirect_to 'http://www.freesendmails.com/test-mail-success'
+        #to mail
+        @to_email = params[:to]
+        #name
+        @name = params[:_name]
+        #email
+        @email = params[:_email]
+        #message email
+        @message = params[:_message]
+        #subject
+        @subject = params[:_subject] ? params[:_subject] : 'Email send by - Free Send Mails'
+        #url sucess
+        @url_success = params[:_url_success]
+
+        #send mail
+        SendMailMailer.send_mail(@to_email, @subject, @email, @message).deliver_later
+
+        if @url_success != nil
+          redirect_to @url_success
+        else
+          redirect_to 'http://www.freesendmails.com/test-mail-success'
+        end
       end
 
     rescue Exception => errors
