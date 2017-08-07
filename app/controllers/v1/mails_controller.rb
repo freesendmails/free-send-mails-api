4658a48ab27cd7dc930f8b60f4da762dd0297b7d
grassiricardo <grassiricardorg@gmail.com>
2017-08-06 23:03:31 -0300

    correction redirect_to

--- app/controllers/v1/mails_controller.rb
+++ app/controllers/v1/mails_controller.rb
@@ -3,12 +3,14 @@ class V1::MailsController < ApplicationController
   #Process create new mail
   def create_mail
     #Using MailService, return url success or error
-    redirect_to = V1::MailService.new(to_email: params[:to],
+    @redirect_to = V1::MailService.new(to_email: params[:to],
       name: params[:_name],
       email: params[:_email],
       message: params[:_message],
       subject: params[:_subject],
       url_success: params[:_url_success]).new_mail
+
+    redirect_to @redirect_to.to_s
   end
 
 end
