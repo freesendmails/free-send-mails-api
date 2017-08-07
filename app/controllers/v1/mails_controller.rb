62249a053081195a81d383b5d511e02312b945bc
grassiricardo <grassiricardorg@gmail.com>
2017-08-06 22:47:35 -0300

    corrections and improvements in process

--- app/controllers/v1/mails_controller.rb
+++ app/controllers/v1/mails_controller.rb
@@ -1,37 +1,14 @@
 class V1::MailsController < ApplicationController
 
+  #Process create new mail
   def create_mail
-    begin
-
-      if params[:to] == "" || params[:_name] == "" || params[:_email] == "" || params[:_message] == ""
-        redirect_to 'http://www.freesendmails.com/test-mail-error'
-      else
-        #to mail
-        @to_email = params[:to]
-        #name
-        @name = params[:_name]
-        #email
-        @email = params[:_email]
-        #message email
-        @message = params[:_message]
-        #subject
-        @subject = params[:_subject] ? params[:_subject] : 'Email send by - Free Send Mails'
-        #url sucess
-        @url_success = params[:_url_success]
-
-        #send mail
-        SendMailMailer.send_mail(@to_email, @subject, @email, @message).deliver_later
-
-        if @url_success != nil
-          redirect_to @url_success
-        else
-          redirect_to 'http://www.freesendmails.com/test-mail-success'
-        end
-      end
-
-    rescue Exception => errors
-      render json: errors, status: :unprocessable_entity
-    end
+    #Using MailService, return url success or error
+    redirect_to = MailService.new(to_email: params[:to],
+      name: params[:_name],
+      email: params[:_email],
+      message: params[:_message],
+      subject: params[:_subject],
+      url_success: params[:_url_success]).new_mail
   end
 
 end
---
+++ app/services/v1/mail_service.rb
@@ -0,0 +1,49 @@
+class V1::MailService
+  #set params
+  def initialize(params)
+    @to_email = params[:to_email]
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    @subject = params[:subject] ? params[:subject] : 'Email send by - Free Send Mails'
+    @url_success = params[:url_success]
+
+    @redirect_to_error = 'http://www.freesendmails.com/test-mail-error'
+    @redirect_to_success = 'http://www.freesendmails.com/test-mail-success'
+  end
+
+  #new_mail
+  def new_mail
+    begin
+      if validation_mail
+        return @redirect_to_error
+      else
+        send_mail
+        return @redirect_to_success
+      end
+    rescue Exception => errors
+      return @redirect_to_error
+    end
+  end
+
+  private
+
+    #Validation of required fields
+    def validation_mail
+      return @to_email == "" || @name == "" || @email == "" || @message == ""
+    end
+
+    #Send mail
+    def send_mail
+      SendMailMailer.send_mail(@to_email, @subject, @email, @message).deliver_later
+    end
+
+    #redirect before send mail
+    def redirect_to_success
+      unless @url_success == nil
+        @url_success = @redirect_to_success
+      end
+
+      return @url_success
+    end
+end
