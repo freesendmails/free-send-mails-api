5688102a2377ffc62583b576be1261cb0def233d
grassiricardo <grassiricardorg@gmail.com>
2017-08-12 00:15:42 -0300

    correction in enviroments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,11 +49,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'ricardograssi91@gmail.com',
-    password:             'r.m,p.a,1',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+    :authentication => :plain,
+    :address => ENV['MAILGUN_SMTP_SERVER'],
+    :port => ENV['MAILGUN_SMTP_PORT'],
+    :domain => ENV['MAILGUN_DOMAIN'],
+    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+    :password => ENV['MAILGUN_SMTP_PASSWORD']
+  }
 end
