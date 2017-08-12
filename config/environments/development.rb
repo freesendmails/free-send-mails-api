7358020b765ac4657e94c6fe27d4999469afb4c3
grassiricardo <grassiricardorg@gmail.com>
2017-08-12 00:28:58 -0300

    build travis

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,9 @@ Rails.application.configure do
   config.active_record.migration_error = :page_load
 
 
+  
+
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
