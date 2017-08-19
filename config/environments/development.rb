27645925d0c54469a191e4b57787353e11e7af48
grassiricardo <grassiricardorg@gmail.com>
2017-08-18 23:17:55 -0300

    add credentials in sidekiq

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.read_encrypted_secrets = true
+  
   # Show full error reports.
   config.consider_all_requests_local = true
 
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,10 @@
+require 'sidekiq'
+require 'sidekiq/web'
+
+Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  [user, password] == [Rails.application.secrets.secret_key_access, Rails.application.secrets.secret_key_password]
+end
+
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://redis:6379/0' }
 end
