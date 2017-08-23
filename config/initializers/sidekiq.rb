41c706599cae19acf387b262edffbb3fee582a37
grassiricardo <grassiricardorg@gmail.com>
2017-08-22 22:27:15 -0300

    alter server redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,9 +6,9 @@ Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis:6379/0' }
+  config.redis = { url: "redis://#{ENV['DATA_QUEUE_HOST']}:6379/5" } 
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis:6379/0' }
+  config.redis = { url: "redis://#{ENV['DATA_QUEUE_HOST']}:6379/5" }
 end
