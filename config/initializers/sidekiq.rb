667b8c5332fa9dab9bf2e402a53636c684ba61a9
grassiricardo <grassiricardorg@gmail.com>
2017-08-30 21:57:30 -0300

    add configurations redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,7 @@ require 'sidekiq'
 require 'sidekiq/web'
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
-  [user, password] == [Rails.application.secrets.secret_key_access, Rails.application.secrets.secret_key_password]
+  [user, password] == ["admin", "123"]
 end
 
 Sidekiq.configure_server do |config|
