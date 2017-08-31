90f54fab220b42ba3207677ebb543382267fd13f
grassiricardo <grassiricardorg@gmail.com>
2017-08-30 22:29:59 -0300

    correction redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,10 @@
 require 'sidekiq'
 require 'sidekiq/web'
 
-Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
-  [user, password] == ["admin", "123"]
-end
-
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV['DATA_QUEUE_HOST']}:6379/5" }
+  config.redis = { url: "#{ENV['REDISTOGO_URL']}:6379/5"}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV['DATA_QUEUE_HOST']}:6379/5" }
+  config.redis = { url: "#{ENV['REDISTOGO_URL']}:6379/5"}
 end
