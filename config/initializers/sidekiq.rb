2110eca611f476b71f10078851f97c46d04747c4
grassiricardo <grassiricardorg@gmail.com>
2017-08-30 21:19:15 -0300

    correction secrets

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,11 +2,11 @@ require 'sidekiq'
 require 'sidekiq/web'
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
-  [user, password] == [ENV['SECRET_KEY_ACCESS'], ENV['SECRET_KEY_PASSWORD']]
+  [user, password] == [Rails.application.secrets.secret_key_access, Rails.application.secrets.secret_key_password]
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV['DATA_QUEUE_HOST']}:6379/5" } 
+  config.redis = { url: "redis://#{ENV['DATA_QUEUE_HOST']}:6379/5" }
 end
 
 Sidekiq.configure_client do |config|
