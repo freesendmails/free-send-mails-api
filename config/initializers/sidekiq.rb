896d76de000111e682486114f006bb10fca0f10b
grassiricardo <grassiricardorg@gmail.com>
2017-08-30 22:39:09 -0300

    add envarioment sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,10 @@
 require 'sidekiq'
 require 'sidekiq/web'
 
+Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  [user, password] == [ENV['SECRET_KEY_ACCESS'], ENV['SECRET_KEY_PASSWORD']]
+end
+
 Sidekiq.configure_server do |config|
   config.redis = { url: "#{ENV['REDISTOGO_URL']}:6379/5"}
 end
