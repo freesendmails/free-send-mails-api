6dbf9505103ee9e43cd3da2f9a86d39031c8dc71
grassiricardo <grassiricardorg@gmail.com>
2017-09-20 22:34:51 -0300

    implements test send mail

--- spec/controllers/v1/mails_controller_spec.rb
+++ spec/controllers/v1/mails_controller_spec.rb
@@ -1,8 +1,23 @@
 require 'rails_helper'
-require 'spec_helper'
+require 'byebug'
 
 RSpec.describe V1::MailsController, type: :controller do
 
   describe "CREATE_MAILS #create_mails" do
+    before do
+      request.env["HTTP_ACCEPT"] = 'application/json'
+    end
+
+    it "send mail" do
+      post :create_mail,
+        params: {
+          to_email: 'mail@freesendmails.com',
+          name: 'Email-Test', email: 'mail@freesendmails.com',
+          message: 'This is test mail',
+          subject: 'Test Mail Controller',
+          url_success: 'https://www.freesendmails.com'
+        }
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+    end
   end
 end
