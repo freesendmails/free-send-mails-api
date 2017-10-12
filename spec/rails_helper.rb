9e8865dc099be522fd74600133c8cf03b42d3928
Diogo Serrano <diogo.serrano@talkdesk.com>
2017-10-12 23:54:26 +0100

    Disable unnecessary code to run specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,18 +3,9 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-# Nós adicionamos o FFaker aqui
-require 'ffaker'
-
-ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # habilita o Jbuilder nos testes
   config.render_views = true
-  # Nós incluimos o Factory Girl Rails aqui
-  config.include FactoryGirl::Syntax::Methods
-  # Aqui nos colocamos os Helpers do Devise para nos ajudar na hora de passar o token
-  config.include Devise::Test::ControllerHelpers, type: :controller
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
