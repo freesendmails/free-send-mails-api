5cc2472547f3799d8bb37994f90f484b9b6bf4a4
Henrique Panham <henriquepjv@hotmail.com>
2017-10-17 19:50:20 -0200

    Feature: insert mailer preview option

--- spec/mailers/previews/send_mail_preview.rb
+++ spec/mailers/previews/send_mail_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/send_mail
 class SendMailPreview < ActionMailer::Preview
-
+  def send_mail
+    SendMailMailer.send_mail('destmock@mail.com',
+                             'Preview Subject',
+                             'freemails@mail.com',
+                             'This is the full message')
+  end
 end
